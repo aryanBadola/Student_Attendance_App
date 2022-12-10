a9b44bf357ddddda8910b831a22e60e09e65eb02
Aryan Badola <aryanbadola431@gmail.com>
2022-12-10 16:40:24 +0530

    1. setOnClickListener for register button and calls PerformAuthentication() method. 2. String emailPattern for validation. 3. Added progressDialog. 4. Implemented the validation inside the performAuthentication() method. 5. requestFocus() method and setError() method. 6. using mAuth.createUserWithEmailAndPassword(...)

--- app/src/main/java/com/example/android/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/android/myapplication/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,6 +13,13 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,6 +27,12 @@ public class RegisterActivity extends AppCompatActivity {
     TextView alreadyHaveAccount;
     EditText inputEmailRegister, inputPasswordRegister, inputConfirmPasswordRegister;
     Button registerButton;
+    ProgressDialog progressDialog;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,9 @@ public class RegisterActivity extends AppCompatActivity {
         inputPasswordRegister=(EditText)findViewById(R.id.input_password_register);
         inputConfirmPasswordRegister=(EditText)findViewById(R.id.confirm_Password_register);
         registerButton=(Button)findViewById(R.id.register_button);
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
 
 
 
@@ -39,6 +58,59 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(new Intent(RegisterActivity.this,MainActivity.class));
             }
         });
+        
+        
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performAuthentication();
+            }
+        });
+
+    }
+
+    private void performAuthentication() {
+        String email=inputEmailRegister.getText().toString();
+        String password=inputPasswordRegister.getText().toString();
+        String confirmPassword=inputConfirmPasswordRegister.getText().toString();
+
+        if(!email.matches(emailPattern))
+        {
+            inputEmailRegister.setError("Enter a valid email");
+            inputEmailRegister.requestFocus();
+        }
+        else if(password.isEmpty() || password.length()<6)
+        {
+            inputPasswordRegister.setError("The password must contain atleast 6 letters");
+            inputPasswordRegister.requestFocus();
+        }
+        else if(!password.matches(confirmPassword))
+        {
+            inputConfirmPasswordRegister.setError("Both the passwords should match");
+        }
+        else
+        {
+            progressDialog.setMessage("Please wait while registering");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
     }
 }
