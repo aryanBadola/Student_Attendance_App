1bcaef0b53343540d77961f5c02e8c6398182f00
Aryan Badola <aryanbadola431@gmail.com>
2022-12-10 13:24:01 +0530

    1. Email and password authentication using firebase. 2. Enable email/password authentication on firebase. 3. Created variables for email, password, confirm password and register button as well.

--- app/src/main/java/com/example/android/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/android/myapplication/RegisterActivity.java
@@ -4,23 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
     TextView alreadyHaveAccount;
+    EditText inputEmailRegister, inputPasswordRegister, inputConfirmPasswordRegister;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        alreadyHaveAccount=(TextView) findViewById(R.id.already_have_account);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        alreadyHaveAccount=(TextView) findViewById(R.id.already_have_account);
+        inputEmailRegister=(EditText)findViewById(R.id.input_Email_register);
+        inputPasswordRegister=(EditText)findViewById(R.id.input_password_register);
+        inputConfirmPasswordRegister=(EditText)findViewById(R.id.confirm_Password_register);
+        registerButton=(Button)findViewById(R.id.register_button);
+
 
 
         alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
