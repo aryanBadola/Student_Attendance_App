9d0a9325a1f769eb68be6b9c54c2753999d7737f
Aryan Badola <aryanbadola431@gmail.com>
2022-12-10 12:13:35 +0530

    1. Added an intent when clicked on Create new account. 2. Added an intent when clicked on Already have an account. 3. For fullscreen used getWindow.setFlags(...)

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNewAccount=(TextView) findViewById(R.id.create_new_account);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/android/myapplication/RegisterActivity.java
@@ -2,13 +2,33 @@ package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    TextView alreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        alreadyHaveAccount=(TextView) findViewById(R.id.already_have_account);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+            }
+        });
+
     }
 }
