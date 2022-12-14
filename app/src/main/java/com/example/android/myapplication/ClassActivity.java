d2ea8c7f53d2711fafea4bbb8ca09de495c53823
Aryan Badola <aryanbadola431@gmail.com>
2022-12-14 18:35:37 +0530

    1. Added a menu item user_profile to the toolbar. 2. OnClicking the menu item it will take it to UserProfileActivity.

--- app/src/main/java/com/example/android/myapplication/ClassActivity.java
+++ app/src/main/java/com/example/android/myapplication/ClassActivity.java
@@ -1,14 +1,47 @@
 package com.example.android.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
 
 public class ClassActivity extends AppCompatActivity {
 
+
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
+
+
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id= item.getItemId();
+                if(id == R.id.action_profile){
+                    startActivity(new Intent(ClassActivity.this,UserProfileActivity.class));
+                }
+                return true;
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        toolbar.inflateMenu(R.menu.user_profile);
+        return true;
+    }
+
+
 }
