46695789f046d73eed14ca489a069d5e83d4d187
Aryan Badola <aryanbadola431@gmail.com>
2022-12-23 00:28:50 +0530

    1. Created the layout for inserting a class.

---
+++ app/src/main/java/com/example/android/myapplication/InsertClassActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_class);
+    }
+}
