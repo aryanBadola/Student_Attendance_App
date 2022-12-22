18ad008475997e07f937e1cb2b2e82feac64ecb9
Aryan Badola <aryanbadola431@gmail.com>
2022-12-22 20:17:11 +0530

    1. Created class_names_adapter.xml for class names. 2. It contains a materialCardView. 3. Inside that it contains frameLayout. 4. Inside that we have imageView and RelativeLayout which further contains three textViews for subjectName, semester and department.

--- app/src/main/java/com/example/android/myapplication/ClassActivity.java
+++ app/src/main/java/com/example/android/myapplication/ClassActivity.java
@@ -3,12 +3,15 @@ package com.example.android.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import java.util.zip.Inflater;
 
@@ -16,6 +19,7 @@ public class ClassActivity extends AppCompatActivity {
 
 
     Toolbar toolbar;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,11 @@ public class ClassActivity extends AppCompatActivity {
 
 
         toolbar=(Toolbar) findViewById(R.id.toolbar);
+        recyclerView=(RecyclerView) findViewById(R.id.class_names_recycler_view);
+
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayout.VERTICAL));
 
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
